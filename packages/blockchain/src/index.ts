b430ebd349285825eb510f488ddcea2b7957ae4b
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-06-27 19:16:00 +0100

    refactor(index): update exports to use ESM .js extensions

--- packages/blockchain/src/index.ts
+++ packages/blockchain/src/index.ts
@@ -1,6 +1,17 @@
-export { AETERNUM_VAULT_ABI, MULTICALL3_ABI } from "./abi";
-export { getVaultAddress, vaultAddresses } from "./addresses";
 export {
-  createPublicClientForRpc,
-  createWalletClientForRpc,
-} from "./client";
+  AETERNUM_VAULT_ABI,
+  MULTICALL3_ABI,
+  MULTICALL3_ADDRESS,
+} from "./abi.js";
+export type { AeternumVaultAbi, Multicall3Abi } from "./abi.js";
+
+export {
+  AETERNUM_VAULT_ADDRESSES,
+  getVaultAddress,
+} from "./addresses.js";
+
+export {
+  createViemPublicClient,
+  createViemWalletClient,
+} from "./client.js";
+export type { ViemPublicClient, ViemWalletClient } from "./client.js";
